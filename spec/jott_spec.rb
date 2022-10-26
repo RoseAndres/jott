7b63485dc7f0b42ad310854ee129ad8a0dfa6904
Andres Rosales <meepman32@gmail.com>
2022-10-26 02:01:59 -0500

    test: fix test

--- spec/jott_spec.rb
+++ spec/jott_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Jott do
+RSpec.describe JottApp do
   it "has a version number" do
-    expect(Jott::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(JottApp.new.has_method(:launch)).to be true
   end
 end
