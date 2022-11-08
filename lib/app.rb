73ad2188a49f2f46960126a26b52492005ddff84
Andres Rosales <meepman32@gmail.com>
2022-11-08 10:03:29 -0600

    refactor: remove a bunch of dead code and clean up requires

--- lib/app.rb
+++ lib/app.rb
@@ -1,28 +1,25 @@
 require "glimmer-dsl-libui"
 
+require "pry"
+require "pry-byebug"
+require "pry-doc"
+
 require "./lib/custom_components/tabs/edit_tab"
 require "./lib/custom_components/tabs/explore_tab"
 require "./lib/custom_components/tabs/settings_tab"
 
+require "./lib/ext/controls"
+
+require "./lib/util"
+
 class App
   include Glimmer
-
-
-  attr_accessor :editing
-  attr_accessor :advanced_settings_enabled, :autosave_period, :workbook_path
 
   def initialize
     create_gui
   end
 
   def launch
-    # TODO move this logic into a side-effect of loading a new file, or autoloading
-    # TODO the last file edited before closing the app
-    # save contents if user isn't in the process of changing
-    # Glimmer::LibUI::timer(autosave_period, repeat: has_content?) do
-    #   save_contents unless editing? || no_content?
-    # end
-
     @main_window.show
   end
 
@@ -30,40 +27,18 @@ class App
 
   def create_gui
     @main_window ||=
-      window("Jätt", 400, 400) {
+      window("Jott") {
+        content_size 600, 400
+        resizable false
+
         tab {
           edit_tab(
             autosave_period: 10,
             file_contents: "some default text"
           )
-          explore_tab
+          explore_tab(config: {}, state: {})
           settings_tab
         }
       }
-  end
-
-  def editing?
-    editing
-  end
-
-  def advanced_settings_disabled?
-    !advanced_settings_enabled?
-  end
-
-  def advanced_settings_enabled?
-    advanced_settings_enabled
-  end
-
-  def has_content?
-    !no_content?
-  end
-
-  def no_content?
-    current_content.nil?
-  end
-
-  # TODO put actual saving logic here eventually
-  def save_contents
-    # save logic
   end
 end
