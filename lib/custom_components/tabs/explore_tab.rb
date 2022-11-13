b6c685ec05811d3f559c7a130b756568a89fddc4
Andres Rosales <meepman32@gmail.com>
2022-11-13 01:24:18 -0600

    feat: add a lot of improvements - notes and workbooks panels that update - auto-deselect workbook if filtering hides it - initializing dummy notes and workbooks

--- lib/custom_components/tabs/explore_tab.rb
+++ lib/custom_components/tabs/explore_tab.rb
@@ -1,18 +1,42 @@
 require "fileutils"
 require "glimmer-dsl-libui"
+require "opt_struct"
 
+require "./lib/custom_components/horizontal_spacer"
 require "./lib/custom_components/vertical_spacer"
+
 require "./lib/ext/string"
 
 class ExploreTab
   include Glimmer::LibUI::CustomControl
 
-  attr_accessor :workbook_entry_text, :create_button_enabled
+  attr_accessor :workbook_entry_text, :create_button_enabled, :notes
 
   DEFAULT_BUTTON_TEXT = "Create new workbook"
+  DEFAULT_RT_FILTER = Glimmer::LibUI::CustomControl::RefinedTable::FILTER_DEFAULT
 
-  option :config
-  option :state
+  BOOKS_AND_NOTES = {
+    first: %w(apples bananas),
+    second: %w(grapes cherries oranges),
+    third: %w(blueberries)
+  }
+
+  Workbook = OptStruct.new do
+    required :name
+    options :notes, delete: "X", open: false
+
+    init { options[:notes] = [] }
+
+    def toggle_open
+      options[:open] = !options[:open]
+    end
+  end
+
+  Note = OptStruct.new do
+    required :name, :text
+
+    init { options[:text] = Faker::Quote.famous_last_words }
+  end
 
   body {
     tab_item("Explore") {
@@ -36,7 +60,7 @@ class ExploreTab
               enabled false
 
               on_clicked do |button|
-                add_workbook(@workbook_entry_text)
+                workbooks << Workbook.new(workbook_entry_text, "⠀", "X")
                 self.workbook_entry_text = ""
                 update_button
               end
@@ -44,21 +68,85 @@ class ExploreTab
           }
         }
 
-        @workbook_labels = vertical_box {
+        horizontal_box {
           stretchy true
 
-          label("first") {
-            stretchy false
-          }
-          horizontal_separator {
-            stretchy false
-          }
+          @workbook_panel = vertical_box {
+            stretchy true
 
-          vertical_spacer
+            vertical_box {
+              @workbook_table = refined_table(
+                model_array: workbooks,
+                table_columns: {
+                  "Name" => {
+                    button: {
+                      on_clicked: -> (row_index) do
+                        select_workbook(@workbook_table.refined_model_array[row_index])
+                      end
+                    }
+                  },
+                  "Open" => :checkbox,
+                  "Delete" => {
+                    button: {
+                      on_clicked: -> (row_index) do
+                        puts workbooks.delete_at(row_index)
+                      end
+                    }
+                  },
+                },
+                table_editable: false,
+                filter: -> (row_hash, query) do
+                  DEFAULT_RT_FILTER.call(row_hash, query).tap do |result|
+                    unless result
+                      @selected_workbook.toggle_open
+                      @selected_workbook = nil
+                    end
+                  end
+                end
+              )
+            }
+          }
+          @notes_panel = vertical_box {
+            stretchy true
+
+            vertical_box {
+              @notes_table = refined_table(
+                model_array: notes,
+                table_columns: {
+                  "Name" => {
+                    button: {
+                      on_clicked: -> (row_index) do
+                        # TODO fix this to work with index
+                        @selected_note = @notes_table.refined_model_array[row_index]
+                        puts @selected_note
+                      end
+                    }
+                  }
+                },
+                table_editable: false
+              ) {
+                stretchy false
+              }
+            }
+          }
         }
       }
     }
   }
+
+  private
+
+  def select_workbook(workbook)
+    @selected_workbook&.toggle_open
+
+    if @selected_workbook != workbook
+      workbook.open = true
+      @selected_workbook = workbook
+
+      notes.clear
+      notes.add_all(workbook.notes.dup)
+    end
+  end
 
   def update_button
     text_present = @workbook_entry_text.present?
@@ -75,16 +163,31 @@ class ExploreTab
     @workbooks ||= load_workbooks
   end
 
-  def add_workbook(workbook_name)
-    @workbook_labels.children.last.destroy
+  def init_notes
+    notes = @selected_workbook.notes.dup
 
-    label = @workbook_labels.create_child_control(:label, workbook_name, add_stretchy: false)
-    separator = @workbook_labels.create_child_control(:horizontal_separator, nil, add_stretchy: false)
-
-    @workbook_labels.content { vertical_spacer }
+    class << notes
+      def add_all(collection)
+        collection.each do |e|
+          self << e
+        end
+      end
+    end
   end
 
   def load_workbooks
-    ["First"]
+    # TODO: load from db
+    [].tap do |arr|
+      BOOKS_AND_NOTES.each do |wb_name, note_names|
+        arr << Workbook.new(name: wb_name).tap do |wb|
+          note_names.each { |name| wb.notes << Note.new(name: name) }
+        end
+      end
+    end.tap do |arr|
+      @selected_workbook = arr.first
+      @selected_workbook.open = true
+
+      init_notes
+    end
   end
 end
