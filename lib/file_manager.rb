228aabbd86852b9ede345a13905ce0f40fc7729d
Andres Rosales <meepman32@gmail.com>
2022-11-18 00:54:14 -0600

    feat: add singleton for reading/writing notes and workbooks

---
+++ lib/file_manager.rb
@@ -0,0 +1,42 @@
+require "fileutils"
+require "singleton"
+
+class FileManager
+  include Singleton
+
+  def create_new_note(workbook_name, note_name)
+    FileUtils.touch(path_to_note(workbook_name, note_name))
+  end
+
+  def create_new_workbook(workbook_name)
+    FileUtils.mkdir_p(path_to_workbook(workbook_name))
+  end
+
+  def get_all_notes_for_workbook(workbook_name)
+    Dir.glob(File.join(path_to_workbook(workbook_name), "*.txt")).
+      select { |p| File.file?(p) }
+  end
+
+  def get_all_workbooks
+    Dir.glob(File.join(base_path, "**")).
+      select { |p| File.directory?(p) }
+  end
+
+  def get_note_contents(workbook_name, note_name)
+    File.open(path_to_note(workbook_name, note_name)).read
+  end
+
+  private
+
+  def base_path
+    File.expand_path("~/jott")
+  end
+
+  def path_to_workbook(workbook_name)
+    File.expand_path(workbook_name, base_path)
+  end
+
+  def path_to_note(workbook_name, note_name)
+    File.expand_path("#{note_name}.txt", path_to_workbook(workbook_name))
+  end
+end
